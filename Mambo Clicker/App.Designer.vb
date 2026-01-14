9a1018b0cbdda38723cd46836fecf5918ecededf
HungNQ2005 <hungnq.ce190870@gmail.com>
2026-01-14 14:45:27 +0700

    Final

--- Mambo Clicker/App.Designer.vb
+++ Mambo Clicker/App.Designer.vb
@@ -24,9 +24,19 @@ Partial Class App
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(App))
         Me.Mambo = New System.Windows.Forms.PictureBox()
-        Me.lblScore = New System.Windows.Forms.Label()
         Me.credit1 = New System.Windows.Forms.LinkLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.layout = New System.Windows.Forms.PictureBox()
+        Me.lblGoal = New System.Windows.Forms.RichTextBox()
+        Me.Stat1 = New System.Windows.Forms.RichTextBox()
+        Me.Stat2 = New System.Windows.Forms.RichTextBox()
+        Me.Stat3 = New System.Windows.Forms.RichTextBox()
+        Me.Stat4 = New System.Windows.Forms.RichTextBox()
+        Me.Stat5 = New System.Windows.Forms.RichTextBox()
+        Me.Stat6 = New System.Windows.Forms.RichTextBox()
+        Me.lblTurn = New System.Windows.Forms.RichTextBox()
         CType(Me.Mambo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.layout, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Mambo
@@ -42,26 +52,137 @@ Partial Class App
         Me.Mambo.UseWaitCursor = True
         Me.Mambo.WaitOnLoad = True
         '
-        'lblScore
-        '
-        Me.lblScore.AutoSize = True
-        Me.lblScore.Location = New System.Drawing.Point(611, 55)
-        Me.lblScore.Name = "lblScore"
-        Me.lblScore.Size = New System.Drawing.Size(39, 13)
-        Me.lblScore.TabIndex = 2
-        Me.lblScore.Text = "Label1"
-        '
         'credit1
         '
+        Me.credit1.ActiveLinkColor = System.Drawing.Color.Peru
         Me.credit1.AutoSize = True
-        Me.credit1.BackColor = System.Drawing.SystemColors.Window
+        Me.credit1.BackColor = System.Drawing.Color.Transparent
         Me.credit1.Font = New System.Drawing.Font("Microsoft Tai Le", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.credit1.Location = New System.Drawing.Point(454, 459)
         Me.credit1.Name = "credit1"
-        Me.credit1.Size = New System.Drawing.Size(226, 14)
+        Me.credit1.Size = New System.Drawing.Size(315, 14)
         Me.credit1.TabIndex = 3
         Me.credit1.TabStop = True
-        Me.credit1.Text = "Fan-made parody game by HungNQ2005, "
+        Me.credit1.Text = "Fan-made parody game by HungNQ2005 (non-commercial)"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(457, 476)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(175, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "All original rights belong to Cygames"
+        '
+        'layout
+        '
+        Me.layout.BackColor = System.Drawing.Color.Transparent
+        Me.layout.BackgroundImage = CType(resources.GetObject("layout.BackgroundImage"), System.Drawing.Image)
+        Me.layout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.layout.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.layout.Location = New System.Drawing.Point(59, 12)
+        Me.layout.Name = "layout"
+        Me.layout.Size = New System.Drawing.Size(355, 495)
+        Me.layout.TabIndex = 5
+        Me.layout.TabStop = False
+        '
+        'lblGoal
+        '
+        Me.lblGoal.Cursor = System.Windows.Forms.Cursors.No
+        Me.lblGoal.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGoal.ForeColor = System.Drawing.Color.Gold
+        Me.lblGoal.Location = New System.Drawing.Point(220, 38)
+        Me.lblGoal.Name = "lblGoal"
+        Me.lblGoal.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.lblGoal.Size = New System.Drawing.Size(124, 16)
+        Me.lblGoal.TabIndex = 6
+        Me.lblGoal.Text = ""
+        '
+        'Stat1
+        '
+        Me.Stat1.Cursor = System.Windows.Forms.Cursors.No
+        Me.Stat1.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Stat1.ForeColor = System.Drawing.Color.Gold
+        Me.Stat1.Location = New System.Drawing.Point(140, 338)
+        Me.Stat1.Name = "Stat1"
+        Me.Stat1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.Stat1.Size = New System.Drawing.Size(23, 15)
+        Me.Stat1.TabIndex = 7
+        Me.Stat1.Text = ""
+        '
+        'Stat2
+        '
+        Me.Stat2.Cursor = System.Windows.Forms.Cursors.No
+        Me.Stat2.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Stat2.ForeColor = System.Drawing.Color.Gold
+        Me.Stat2.Location = New System.Drawing.Point(180, 338)
+        Me.Stat2.Name = "Stat2"
+        Me.Stat2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.Stat2.Size = New System.Drawing.Size(23, 15)
+        Me.Stat2.TabIndex = 8
+        Me.Stat2.Text = ""
+        '
+        'Stat3
+        '
+        Me.Stat3.Cursor = System.Windows.Forms.Cursors.No
+        Me.Stat3.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Stat3.ForeColor = System.Drawing.Color.Gold
+        Me.Stat3.Location = New System.Drawing.Point(220, 338)
+        Me.Stat3.Name = "Stat3"
+        Me.Stat3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.Stat3.Size = New System.Drawing.Size(23, 15)
+        Me.Stat3.TabIndex = 9
+        Me.Stat3.Text = ""
+        '
+        'Stat4
+        '
+        Me.Stat4.Cursor = System.Windows.Forms.Cursors.No
+        Me.Stat4.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Stat4.ForeColor = System.Drawing.Color.Gold
+        Me.Stat4.Location = New System.Drawing.Point(264, 338)
+        Me.Stat4.Name = "Stat4"
+        Me.Stat4.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.Stat4.Size = New System.Drawing.Size(23, 15)
+        Me.Stat4.TabIndex = 10
+        Me.Stat4.Text = ""
+        '
+        'Stat5
+        '
+        Me.Stat5.Cursor = System.Windows.Forms.Cursors.No
+        Me.Stat5.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Stat5.ForeColor = System.Drawing.Color.Gold
+        Me.Stat5.Location = New System.Drawing.Point(307, 338)
+        Me.Stat5.Name = "Stat5"
+        Me.Stat5.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.Stat5.Size = New System.Drawing.Size(23, 15)
+        Me.Stat5.TabIndex = 11
+        Me.Stat5.Text = ""
+        '
+        'Stat6
+        '
+        Me.Stat6.Cursor = System.Windows.Forms.Cursors.No
+        Me.Stat6.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Stat6.ForeColor = System.Drawing.Color.Black
+        Me.Stat6.Location = New System.Drawing.Point(352, 338)
+        Me.Stat6.Name = "Stat6"
+        Me.Stat6.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.Stat6.Size = New System.Drawing.Size(23, 15)
+        Me.Stat6.TabIndex = 12
+        Me.Stat6.Text = ""
+        '
+        'lblTurn
+        '
+        Me.lblTurn.Cursor = System.Windows.Forms.Cursors.No
+        Me.lblTurn.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTurn.ForeColor = System.Drawing.Color.Gold
+        Me.lblTurn.Location = New System.Drawing.Point(140, 47)
+        Me.lblTurn.Name = "lblTurn"
+        Me.lblTurn.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lblTurn.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.lblTurn.Size = New System.Drawing.Size(36, 18)
+        Me.lblTurn.TabIndex = 13
+        Me.lblTurn.Text = ""
         '
         'App
         '
@@ -71,8 +192,17 @@ Partial Class App
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(886, 501)
+        Me.Controls.Add(Me.lblTurn)
+        Me.Controls.Add(Me.Stat6)
+        Me.Controls.Add(Me.Stat5)
+        Me.Controls.Add(Me.Stat4)
+        Me.Controls.Add(Me.Stat3)
+        Me.Controls.Add(Me.Stat2)
+        Me.Controls.Add(Me.Stat1)
+        Me.Controls.Add(Me.lblGoal)
+        Me.Controls.Add(Me.layout)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.credit1)
-        Me.Controls.Add(Me.lblScore)
         Me.Controls.Add(Me.Mambo)
         Me.DoubleBuffered = True
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -80,12 +210,22 @@ Partial Class App
         Me.Name = "App"
         Me.Text = "Umamusume: Mambo Clicker"
         CType(Me.Mambo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.layout, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Mambo As PictureBox
-    Friend WithEvents lblScore As Label
     Friend WithEvents credit1 As LinkLabel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents layout As PictureBox
+    Friend WithEvents lblGoal As RichTextBox
+    Friend WithEvents Stat1 As RichTextBox
+    Friend WithEvents Stat2 As RichTextBox
+    Friend WithEvents Stat3 As RichTextBox
+    Friend WithEvents Stat4 As RichTextBox
+    Friend WithEvents Stat5 As RichTextBox
+    Friend WithEvents Stat6 As RichTextBox
+    Friend WithEvents lblTurn As RichTextBox
 End Class
--- Mambo Clicker/App.vb
+++ Mambo Clicker/App.vb
@@ -1,6 +1,22 @@
 ﻿Imports System.Media
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class App
+
+    ' =========================================================
+    ' Project : Umamusume: Mambo Clicker (Parody Fan Project)
+    ' Author  : HungNQ2005
+    '
+    ' Disclaimer:
+    ' This is a fan-made parody project created for fun and
+    ' educational purposes only. All original characters,
+    ' visual styles, and intellectual property referenced or
+    ' inspired by Umamusume: Pretty Derby are the property of
+    ' Cygames, Inc.
+    '
+    ' Non-commercial use only.
+    ' =========================================================
+
 
     ' ========================
     ' GAME DATA
@@ -19,17 +35,40 @@ Public Class App
 
     ' ========================
     ' FORM LOAD.
-    ' ========================
+    ' == ======================
     Private Sub App_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.MaximizeBox = False
         Me.MinimumSize = Me.Size
         Me.MaximumSize = Me.Size
+        lblGoal.BorderStyle = BorderStyle.None
+        Stat1.BorderStyle = BorderStyle.None
+        Stat2.BorderStyle = BorderStyle.None
+        Stat3.BorderStyle = BorderStyle.None
+        Stat4.BorderStyle = BorderStyle.None
+        Stat5.BorderStyle = BorderStyle.None
+        Stat6.BorderStyle = BorderStyle.None
+        lblTurn.BorderStyle = BorderStyle.None
+
 
         ' PictureBox setup
         Mambo.SizeMode = PictureBoxSizeMode.Zoom
+        layout.SizeMode = PictureBoxSizeMode.Zoom
+        layout.Parent = Mambo
+        layout.BackColor = Color.Transparent
+        layout.Location = New Point(20, 10)
+        layout.BringToFront()
+
+
+
 
         ' Score
-        lblScore.Text = "Score: 0"
+        lblGoal.Text = "Goal: Touch Mambo"
+        lblGoal.Text = "1"
+        Stat1.Text = "0"
+        Stat2.Text = "0"
+        Stat3.Text = "0"
+        Stat4.Text = "0"
+        Stat5.Text = "0"
 
         ' Save original size & position
         originalSize = Mambo.Size
@@ -40,9 +79,9 @@ Public Class App
         popTimer.Interval = 80 ' ms
         AddHandler popTimer.Tick, AddressOf PopBack
 
-        ' Init sound (SAFE PATH)
+        ' Init sound 
         Dim soundPath As String =
-            IO.Path.Combine(Application.StartupPath, "Sources\Mambo.wav")
+            IO.Path.Combine(Application.StartupPath, "SRC\Mambo.wav")
 
         If Not IO.File.Exists(soundPath) Then
             MessageBox.Show("Không tìm thấy file:" & vbCrLf & soundPath,
@@ -59,22 +98,7 @@ Public Class App
     ' CLICK MAMBO
     ' ========================
     Private Sub Mambo_Click(sender As Object, e As EventArgs) Handles Mambo.Click
-
-        ' Anti spam click
-        If (DateTime.Now - lastClick).TotalMilliseconds < 200 Then Exit Sub
-        lastClick = DateTime.Now
-
-        ' POP EFFECT
-        PopMambo()
-
-        ' Play sound
-        mamboSound.Stop()
-        mamboSound.Play()
-
-        ' +1 score
-        score += 1
-        lblScore.Text = "Score: " & score.ToString()
-
+        DoMambo()
     End Sub
 
     ' ========================
@@ -111,7 +135,7 @@ Public Class App
     End Sub
 
     Private Sub credit1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles credit1.LinkClicked
-        Dim url As String = "https://github.com/HungNQ2005/ten-repo-cua-ban"
+        Dim url As String = "https://github.com/HungNQ2005/Umamusume-Mambo-Clicker"
 
         Try
             Process.Start(New ProcessStartInfo(url) With {.UseShellExecute = True})
@@ -119,5 +143,34 @@ Public Class App
             MessageBox.Show("Cannot open link.", "Error",
                             MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
+    End Sub
+
+    Private Sub layout_Click(sender As Object, e As EventArgs) Handles layout.Click
+        DoMambo()
+    End Sub
+
+
+    Sub DoMambo()
+        ' Anti spam click
+        If (DateTime.Now - lastClick).TotalMilliseconds < 200 Then Exit Sub
+        lastClick = DateTime.Now
+
+        ' POP EFFECT
+        PopMambo()
+
+        ' Play sound
+        mamboSound.Stop()
+        mamboSound.Play()
+
+        ' You will never reach the goal hahaha
+        score += 1
+        lblGoal.Text = "Reach " & 1 + score.ToString() & " in every stats."
+        Stat1.Text = score.ToString()
+        Stat2.Text = score.ToString()
+        Stat3.Text = score.ToString()
+        Stat4.Text = score.ToString()
+        Stat5.Text = score.ToString()
+        Stat6.Text = score.ToString()
+        lblTurn.Text = 2 + score.ToString()
     End Sub
 End Class
